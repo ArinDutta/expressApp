ce887ab00b7858d5662fbba00f0d8559054bed4d
c15436 <c15436@P50-PC0DZ13Q.corp.us.int.rci.com>
2020-01-10 11:50:00 -0500

    Update index js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app=express()
 const log=require('./config/logger')
 var routes=require('./route/route')
-const port=3000
+const port=8080
 let logger=new log.LoggerFunction()
 app.use(logger.logRequest)
 app.use('/employee',routes)
